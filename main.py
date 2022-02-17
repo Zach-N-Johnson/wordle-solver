58d8d99b00919afbf27c6c451977393d209c6d19
Zach Johnson <johnson.zachary2002@gmail.com>
2022-02-16 22:41:19 -0600

    remove wrong elements

--- main.py
+++ main.py
@@ -4,21 +4,32 @@ import utils
 clear = lambda: os.system('cls')
 
 def main():
+    guess = ""
     guesses = 0
-    answers = utils.get_list()
-    freq = utils.get_frequencies()
+    answers = utils.getList()
+    freq = utils.getFrequencies()
     word = ['s', 'l', 'a', 't', 'e']
     clear()
 
     print("Wordle Solver\nInput G for green\nInput Y for yellow\nInput N for gray")
     while(True):
+        for i in answers:
+            print(utils.toString(i))
+        print(f"{len(answers)} options")
         print(f"\nTry {utils.toString(word)}")
-        if(utils.valid_entry()):
+
+        print("Enter your guess")
+        guess = utils.toList(utils.validGuess())
+
+        print("Enter colors")
+        colors = utils.validColors()
+        if colors == "GGGGG" or colors == "ggggg":
             print(f"Solved in {guesses+1} guesses")
             break
+        colors = utils.toList(colors)
 
+        answers = utils.checkFull(guess, colors, answers)
 
-        
         guesses += 1
         
 
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import json
+from re import L
 
-def get_list():
+def getList():
     file = open("data/answers.txt", "r")
 
     answers = []
@@ -11,13 +12,13 @@ def get_list():
 
     return answers
 
-def get_frequencies():
+def getFrequencies():
     with open("data/frequencies.txt") as f:
         freq = f.read()
 
     return json.loads(freq)
 
-def valid_entry():
+def validColors():
     while(True):
         not_ok = False
         result = input()
@@ -30,14 +31,61 @@ def valid_entry():
             print("Response must only use G, Y, or N")
         else:
             break
-    if(result == "GGGGG" or result == "ggggg"):
-        return True
-    else:
-        return False
+    return result
+
+def validGuess():
+    while(True):
+        guess = input()
+        if(len(guess) != 5):
+            print("Response must be five letters")
+        else:
+            break
+    return guess
 
 def toString(word):
     new_word = ""
     for i in word:
         new_word += i
-    
     return new_word
+
+def toList(word):
+    return list(word)
+
+def checkFull(word, colors, list):
+    k = 0
+    for i in colors:
+        if i == 'G' or i == 'g':
+            list = checkGreen(k, word[k], list)
+        elif i == 'Y' or i == 'y':
+            list = checkYellow(k, word[k], list)
+        else:
+            list = checkGray(word[k], list)
+        k += 1
+    return list
+
+def checkGreen(pos, letter, list):
+    for i in reversed(list):
+        if not(i[pos] == letter):
+            list.remove(i)
+    return list
+
+def checkYellow(pos, letter, list):
+    for i in reversed(list):
+        check = True
+        if i[pos] == letter:
+            list.remove(i)
+        else:
+            for j in i:
+                if j == letter:
+                    check = False
+            if check:
+                list.remove(i)
+    return list
+
+def checkGray(letter, list):
+    for i in reversed(list):
+        for j in i:
+            if j == letter:
+                list.remove(i)
+                break
+    return list

00092ebe1eeef4eaf643d79939c5a0cfa0636e66
Zach Johnson <johnson.zachary2002@gmail.com>
2022-02-21 17:25:01 -0600

    working but bad

--- main.py
+++ main.py
@@ -6,37 +6,87 @@ clear = lambda: os.system('cls')
 def main():
     guess = ""
     guesses = 0
-    answers = utils.getList()
-    freq = utils.getFrequencies()
-    word = ['s', 'l', 'a', 't', 'e']
+    answers = utils.getAnswers()
+    freq = utils.getFreqs(answers)
+    words = utils.getTopFive(answers, freq)
     clear()
 
-    print("Wordle Solver\nInput G for green\nInput Y for yellow\nInput N for gray")
+    print("Wordle Solver\nInput G for green, Y for yellow, and N for gray")
     while(True):
-        for i in answers:
+        print("Top five options:")
+        for i in words:
             print(utils.toString(i))
-        print(f"{len(answers)} options")
-        print(f"\nTry {utils.toString(word)}")
 
-        print("Enter your guess")
+        print("\nEnter your guess")
         guess = utils.toList(utils.validGuess())
 
-        print("Enter colors")
+        print("\nEnter colors")
         colors = utils.validColors()
         if colors == "GGGGG" or colors == "ggggg":
             print(f"Solved in {guesses+1} guesses")
             break
+        
         colors = utils.toList(colors)
-
         answers = utils.checkFull(guess, colors, answers)
-
-        word = utils.getNewWord(answers, freq)
-
+        freq = utils.getFreqs(answers)
+        words = utils.getTopFive(answers, freq)
         guesses += 1
         
+        clear()
+        print(f"{len(answers)} options left")
 
     new_wordle = input("New Wordle? (Y, N)\n")
     if(new_wordle == "Y" or new_wordle == "y"):
         main()
 
-main()
+def getAverage():
+    guesses = 0
+    answers = utils.getAnswers()
+    total1 = total2 = total3 = total4 = total5 = total6 = totalx = 0
+    loops = 0
+
+    for i in answers:
+        thisGuesses = 0
+        freq = utils.getFreqs(answers)
+        thisAnswers = utils.getAnswers()
+        while(True):
+            guess = utils.getNewWord(thisAnswers, freq)
+            colors = utils.getColors(i, guess)
+            if utils.toString(colors) == "GGGGG" or utils.toString(colors) == "ggggg":
+                thisGuesses += 1
+                if loops % 50 == 0:
+                    print(loops)
+                if thisGuesses == 1:
+                    total1 += 1
+                elif thisGuesses == 2:
+                    total2 += 1
+                elif thisGuesses == 3:
+                    total3 += 1
+                elif thisGuesses == 4:
+                    total4 += 1
+                elif thisGuesses == 5:
+                    total5 += 1
+                elif thisGuesses == 6:
+                    total6 += 1
+                else:
+                    totalx += 1
+                guesses += thisGuesses
+                loops += 1
+                break
+            
+            thisAnswers = utils.checkFull(guess, colors, thisAnswers)
+            freq = utils.getFreqs(thisAnswers)
+            thisGuesses += 1
+
+    print(f"\n\n\n\nTotal guesses - {guesses}")
+    print(f"Average - {guesses/2315}")
+    print(f"Total 1 - {total1}")
+    print(f"Total 2 - {total2}")
+    print(f"Total 3 - {total3}")
+    print(f"Total 4 - {total4}")
+    print(f"Total 5 - {total5}")
+    print(f"Total 6 - {total6}")
+    print(f"Total x - {totalx}")
+        
+
+getAverage()
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
-import json
-
-def getList():
+# get answer list
+def getAnswers():
     file = open("data/answers.txt", "r")
 
     answers = []
@@ -11,12 +10,7 @@ def getList():
 
     return answers
 
-def getFrequencies():
-    with open("data/revised_frequencies.txt") as f:
-        freq = f.read()
-
-    return json.loads(freq)
-
+# check if color entry is valid, return input
 def validColors():
     while(True):
         not_ok = False
@@ -32,6 +26,7 @@ def validColors():
             break
     return result
 
+# check if guess entry is valid, return guess
 def validGuess():
     while(True):
         guess = input()
@@ -41,17 +36,22 @@ def validGuess():
             break
     return guess
 
+# convert list to string
 def toString(word):
     new_word = ""
     for i in word:
         new_word += i
     return new_word
 
+# convert string to list
 def toList(word):
     return list(word)
 
+# if checked green before, only remove gray in that position
+# return new list with only the possible words left after guess
 def checkFull(word, colors, list):
     k = 0
+    dups = checkDup(word)
     for i in colors:
         if i == 'G' or i == 'g':
             list = checkGreen(k, word[k], list)
@@ -62,12 +62,14 @@ def checkFull(word, colors, list):
         k += 1
     return list
 
+# return list after green check
 def checkGreen(pos, letter, list):
     for i in reversed(list):
         if not(i[pos] == letter):
             list.remove(i)
     return list
 
+# return list after yellow check
 def checkYellow(pos, letter, list):
     for i in reversed(list):
         check = True
@@ -81,6 +83,7 @@ def checkYellow(pos, letter, list):
                 list.remove(i)
     return list
 
+# return list after gray check
 def checkGray(letter, list):
     for i in reversed(list):
         for j in i:
@@ -89,34 +92,95 @@ def checkGray(letter, list):
                 break
     return list
 
+# check for duplicates in a list, return the duplicate item
 def checkDup(list):
     if len(list) == len(set(list)):
         return None
     return getDup(list)
 
+# helper for checkDup
 def getDup(list):
     l = []
+    dups = []
     for i in list:
         if i not in l:
             l.append(i)
         else:
-            return i
-    return None
+            dups.append(i)
+    return dups
 
-def getFreqs():
-    answers = getList()
-    total = 11575
+# get new frequency data for answer list by percentage
+def getFreqs(list):
+    answers = list
+    total = len(list) * 5
     freqs = dict.fromkeys(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], 0)
     for i in answers:
         for j in i:
             freqs[j] += 1
-        if checkDup(i) != None:
-            freqs[checkDup(i)] -= 1
+        if checkDup(i):
+            freqs[checkDup(i)[0]] -= 1
             total -= 1
+        if checkDup(i) and len(checkDup(i)) == 2:
+            freqs[checkDup(i)[1]] -= 1
     for i in freqs:
         freqs[i] /= total
-    print(dict(sorted(freqs.items(), key=lambda x: x[1], reverse=True)))
+    return dict(sorted(freqs.items(), key=lambda x: x[1], reverse=True))
 
+# use new freq data to return word with highest possible freq
 def getNewWord(answers, freq):
-    # TODO
-    return
+    answersfreq = []
+    for i in answers:
+        totalfreq = 0
+        dups = checkDup(i)
+        for j in i:
+            totalfreq += freq[j]
+        if checkDup(i):
+            totalfreq -= freq[checkDup(i)[0]]
+        if checkDup(i) and len(checkDup(i)) == 2:
+            totalfreq -= freq[checkDup(i)[1]]
+        answersfreq.append(totalfreq)
+    answersfreq, answers = (list(t) for t in zip(*sorted(zip(answersfreq, answers))))
+    return answers[-1]
+
+# use new freq data to return list of five words with highest possible freq
+def getTopFive(answers, freq):
+    answersfreq = []
+    topfive = []
+    count = 0
+
+    for i in answers:
+        totalfreq = 0
+        for j in i:
+            totalfreq += freq[j]
+        if checkDup(i):
+            totalfreq -= freq[checkDup(i)[0]]
+        if checkDup(i) and len(checkDup(i)) == 2:
+            totalfreq -= freq[checkDup(i)[1]]
+        answersfreq.append(totalfreq)
+    answersfreq, answers = (list(t) for t in zip(*sorted(zip(answersfreq, answers))))
+    for i in reversed(answers):
+        if count == 5:
+            break
+        else:
+            topfive.append(i)
+            count += 1
+    return topfive
+
+def getColors(word, guess):
+    colors = []
+    yellow = False
+    dup = getDup(word)
+    for i in range(0, 5):
+        if word[i] == guess[i]:
+            colors.append("G")
+        else:
+            for j in range(0, 5):
+                if word[j] == guess[i]:
+                    yellow = True
+                    break
+            if yellow:
+                colors.append("Y")
+            else:
+                colors.append("N")
+        yellow = False
+    return colors
